20169288ecddb4ae6a6e2466bd6c778abbcadf6d
Zach Engstrom <zach.engstrom@bluecrossmn.com>
2018-11-13 15:13:32 -0600

    had to rearrange the order of JS files being concatenated to make navbar dropdown and toggle work

--- src/js/vendor-scripts.js
+++ src/js/vendor-scripts.js
@@ -1,5 +1,5 @@
+//@prepros-append vendor/popper.js
+//@prepros-append vendor/bootstrap.js
 //@prepros-append vendor/customizer.js
 //@prepros-append vendor/navigation.js
 //@prepros-append vendor/skip-link-focus-fix.js
-//@prepros-append vendor/popper.js
-//@prepros-append vendor/bootstrap.js
