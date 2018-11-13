62ad3b003f759560fa7614fad547968c06ddea83
Zach Engstrom <zach.engstrom@bluecrossmn.com>
2018-11-13 16:55:09 -0600

    header.php - called site name and link in the header navbar

--- header.php
+++ header.php
@@ -26,7 +26,26 @@
 
   <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="site-branding">
+        <?php
+        the_custom_logo();
+        if ( is_front_page() && is_home() ) :
+          ?>
+          <h1 class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php
+        else :
+          ?>
+          <p class="h1 site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+          <?php
+        endif;
+        $zachengstrom_description = get_bloginfo( 'description', 'display' );
+        if ( $zachengstrom_description || is_customize_preview() ) :
+          ?>
+          <p class="site-description d-none"><?php echo $zachengstrom_description; /* WPCS: xss ok. */ ?></p>
+        <?php endif; ?>
+
+      </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
