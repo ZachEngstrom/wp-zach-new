e993aab08879b3ec67441ecdc67a478bd5f0345b
Zach Engstrom <zach.engstrom@bluecrossmn.com>
2018-11-14 16:22:35 -0600

    change header navbar color when scrolling

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,20 @@
-$(function(){
-  // Code here
+$(function() {
+
+  /**
+   * Change Navbar color when scrolling
+   * @link http://lukedowding.com/change-nav-bar-menu-add-css-class-scroll/
+   * @link https://codepen.io/LukeD1uk/pen/zvGQZN
+   */
+  var header = $(".navbar");
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 75) {
+      header.addClass("navbar-dark bg-dark");
+      header.removeClass("navbar-light bg-light");
+    } else {
+      header.addClass("navbar-light bg-light");
+      header.removeClass("navbar-dark bg-dark");
+    }
+  });
+
 });
