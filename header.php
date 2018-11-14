64f51603b6e91defe7e0efb5a4b5899781a2295f
Zach Engstrom <zach.engstrom@bluecrossmn.com>
2018-11-14 16:20:26 -0600

    header.php - moved header element from inside the nav element to wrap the nav element

--- header.php
+++ header.php
@@ -24,67 +24,35 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zachengstrom' ); ?></a>
 
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+  <header id="masthead" class="site-header container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
 
-      <div class="site-branding">
-        <?php
-        the_custom_logo();
-        if ( is_front_page() && is_home() ) :
-          ?>
-          <h1 class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <div class="site-branding">
           <?php
-        else :
-          ?>
-          <p class="h1 site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-          <?php
-        endif;
-        $zachengstrom_description = get_bloginfo( 'description', 'display' );
-        if ( $zachengstrom_description || is_customize_preview() ) :
-          ?>
-          <p class="site-description d-none"><?php echo $zachengstrom_description; /* WPCS: xss ok. */ ?></p>
-        <?php endif; ?>
+          the_custom_logo();
+          if ( is_front_page() && is_home() ) :
+            ?>
+            <h1 class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php
+          else :
+            ?>
+            <p class="h1 site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            <?php
+          endif;
+          $zachengstrom_description = get_bloginfo( 'description', 'display' );
+          if ( $zachengstrom_description || is_customize_preview() ) :
+            ?>
+            <p class="site-description d-none"><?php echo $zachengstrom_description; /* WPCS: xss ok. */ ?></p>
+          <?php endif; ?>
 
-      </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <?php if (function_exists(zachengstromcustom_menu_header_primary())) zachengstromcustom_menu_header_primary(); ?>
-      </div>
-    </nav>
-  </div>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$zachengstrom_description = get_bloginfo( 'description', 'display' );
-			if ( $zachengstrom_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $zachengstrom_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zachengstrom' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <?php if (function_exists(zachengstromcustom_menu_header_primary())) zachengstromcustom_menu_header_primary(); ?>
+        </div>
+      </nav>
+  </header>
 
 	<div id="content" class="site-content">
